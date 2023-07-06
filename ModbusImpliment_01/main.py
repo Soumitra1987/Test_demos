f89be29e5a0310d6c8993cdcaf92e153dd2b2a46
Soumitra1987 <soumitra1015@gmail.com>
2023-07-06 23:56:13 +0530

    Update main.py

--- ModbusImpliment_01/main.py
+++ ModbusImpliment_01/main.py
@@ -2,11 +2,37 @@
 
 import socket
 import serial
+import struct
 import keyboard
 import time
 
 #     if Mode = 1 then it is serial or Mode =2 then it is TCPIP
 Mode = int(input("Enter the Mode:"))
+
+
+'''def Modbusparameters(self,unitID,funion):
+    # Set the Modbus parameters
+    unitId = 16
+    functionCode = 3  # Read Holding Registers
+    startAddress = 40010 - 1  # Subtract 1 because Modbus addresses start at 0
+    numRegisters = 1
+
+    # Create the Modbus request
+    req = struct.pack('>HHHBBHH', 0, 0, 6, unitId, functionCode, startAddress, numRegisters)
+
+    # Send the request to the Modbus device
+    sock.send(req)
+
+    # Receive the response from the Modbus device
+    response = sock.recv(1024)
+
+    # Unpack the response to get the register value
+    header, data = response[:7], response[7:]
+    values = struct.unpack('>' + 'H' * numRegisters, data[2:])
+
+    # Print the register value
+    print(values)'''
+
 
 
 def ser1():
@@ -15,13 +41,30 @@ def ser1():
     ser = serial.Serial(COM, Baud, timeout=1)
 
     while True:
-        time.sleep(5)
-        # Read holding register 0x00 from slave 1
-        request = b'\x01\x03\x00\x00\x00\x01\x84\x0A'
-        ser.write(request)
-        response = ser.read(5)
 
-        print(response)
+        # Read holding register 0x00 from slave 1
+        unitId = 1
+        functionCode = 3  # Read Holding Registers
+        startAddress = 40002 - 1  # Subtract 1 because Modbus addresses start at 0
+        numRegisters = 2
+
+        # Create the Modbus request
+        request = struct.pack('>HHHBBHH', 0, 0, 6, unitId, functionCode, startAddress, numRegisters)
+
+        # Send the request to the Modbus device
+        ser.write(request)
+
+        # Receive the response from the Modbus device
+        response = ser.read(8)
+
+        # Unpack the response to get the register value
+        header, data = response[:7], response[7:]
+        values = struct.unpack('>' + 'H'* numRegisters, data[2:])
+
+        # Print the register value
+        print(values)
+
+        time.sleep(2)
 
         if keyboard.is_pressed("Q"):
             print("User want to Quit")
@@ -47,17 +90,34 @@ def TCP1():
         time.sleep(2)
 
         # send data to the server
-        modtcp.sendall(b'\x00\x01\x00\x00\x00\x06\x01\x03\x00\x00\x00\x01')
+        unitId = 1
+        functionCode = 3  # Read Holding Registers
+        startAddress = 40010 - 1  # Subtract 1 because Modbus addresses start at 0
+        numRegisters = 1
+
+        # Create the Modbus request
+        request = struct.pack('>HHHBBHH', 0, 0, 6, unitId, functionCode, startAddress, numRegisters)
+
+        # Send the request to the Modbus device
+        modtcp.send(request)
+
+        # Receive the response from the Modbus device
+        response = modtcp.recv(1024)
+
+        # Unpack the response to get the register value
+        header, data = response[:7], response[7:]
+        values = struct.unpack('>' + 'H' * numRegisters, data[2:])
+
+        # Print the register value
+        print(values)
 
         # receive data from the server
-        data = modtcp.recv(1024)
-
-        print((data))
 
         if keyboard.is_pressed("Q"):
             print("User want to Quit")
             break
         print("Connected")
+    modtcp.close()
 
 
 if Mode == 1:  # serial connection
